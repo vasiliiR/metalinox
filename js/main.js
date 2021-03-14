c6cc792628e7ace2e23bc4f4a37fc498db7f4bfd
vasilii <wasiok123@gmail.com>
2021-03-14 17:34:31 +0200

    adaptive header and slider done

--- js/main.js
+++ js/main.js
@@ -1,10 +1,31 @@
 $(document).ready(function(){
     $('.your-class').slick({
         dots: true,
+				dotsClass: "my-dots",
         arrows: false,
+				autoplay: true,
+				adaptiveHeight: true,
+				speed: 500,
+
+			responsive: [
+				{
+					breakpoint: 768,
+					settings: {
+						slidesToShow: 1,
+					}
+				},
+				{
+					breakpoint: 480,
+					settings: {
+						slidesToShow: 1,
+					}
+				}
+			]
     });
   });
 
   $('.slider').on('afterChange',function(e, slick, currentSlide){
     slick.$dots.attr('data-current-slide', currentSlide);
 });
+
+
