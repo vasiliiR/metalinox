36830ffc95ff0f7420152c89c06e1cb5c7d3c33e
vasiliiR <wasiok123@gmail.com>
2021-03-15 12:01:32 +0200

    header end with responsive

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
     $('.your-class').slick({
-        dots: true,
+        		dots: true,
 				dotsClass: "my-dots",
-        arrows: false,
+        		arrows: false,
 				autoplay: true,
 				adaptiveHeight: true,
 				speed: 500,
@@ -18,6 +18,7 @@ $(document).ready(function(){
 					breakpoint: 480,
 					settings: {
 						slidesToShow: 1,
+						dots: false,
 					}
 				}
 			]
@@ -28,4 +29,35 @@ $(document).ready(function(){
     slick.$dots.attr('data-current-slide', currentSlide);
 });
 
+$('#my_Dropdown').click(function() {
 
+        $('.dropdown-content').hide('dropdown-content');
+});
+
+$( "#myInput" ).click(function() {
+	$( ".dropdown-content" ).show( 500 );
+  });
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+	document.getElementById("my_Dropdown").classList.toggle("show");
+  }
+  
+  function filterFunction() {
+	var input, filter, ul, li, a, i;
+	input = document.getElementById("myInput");
+	filter = input.value.toUpperCase();
+	div = document.getElementById("my_Dropdown");
+	a = div.getElementsByTagName("a");
+	for (i = 0; i < a.length; i++) {
+	  txtЗначение = a[i].textСодержание || a[i].innerText;
+	  if (txtЗначение.toUpperCase().indexOf(filter) > -1) {
+		a[i].style.display = "";
+	  } else {
+		a[i].style.display = "none";
+	  }
+	}
+
+	
+  }
